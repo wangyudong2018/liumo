50ecb8ac96767badd87d4f9dd6c777324664c5b9
FFelix <13314209626@163.com>
2018-06-14 17:24:20 +0800

    新增 Header 组件

---
+++ pc/src/components/header/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <ul>
+      <li><router-link to="/">首页</router-link></li>
+      <li><router-link to="/service">精选服务</router-link></li>
+      <li><router-link to="/news">新闻中心</router-link></li>
+      <li><router-link to="/about">了解六漠</router-link></li>
+    </ul>
+  </div>
+</template>
