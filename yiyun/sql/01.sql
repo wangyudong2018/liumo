9a0d48877f949c7c5458c38ce269fef182e976a7
wangyudong <wangyudongjava@163.com>
2018-07-08 14:22:08 +0800

    sql

--- yiyun/sql/01.sql
+++ yiyun/sql/01.sql
@@ -1,44 +1,92 @@
 USE `yiyun`;
 
+/*Table structure for table `lm_banner` */
+
+DROP TABLE IF EXISTS `lm_banner`;
+
+CREATE TABLE `lm_banner` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `channel` varchar(10) DEFAULT NULL COMMENT '渠道',
+  `page` varchar(20) DEFAULT NULL COMMENT '属于哪个页面的banner',
+  `file_id` bigint(20) DEFAULT NULL COMMENT '文件表ID',
+  `sort` int(11) DEFAULT NULL COMMENT '排序',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='六漠banner表';
+
+/*Table structure for table `lm_file` */
+
 DROP TABLE IF EXISTS `lm_file`;
 
 CREATE TABLE `lm_file` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `lm_type` varchar(50) DEFAULT NULL COMMENT '类型',
   `lm_file` mediumblob COMMENT '文件',
-  `sort` int(11) DEFAULT NULL COMMENT '排序',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='六漠文件表';;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='六漠文件表';
+
+/*Table structure for table `lm_product` */
 
 DROP TABLE IF EXISTS `lm_product`;
 
 CREATE TABLE `lm_product` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `prd_type` varchar(10) DEFAULT NULL COMMENT '产品类型',
   `title` varchar(100) DEFAULT NULL COMMENT '产品名称',
   `content` varchar(2000) DEFAULT NULL COMMENT '产品介绍',
-  `agreemt` varchar(100) DEFAULT NULL COMMENT '居间服务',
-  `rate` varchar(50) DEFAULT NULL COMMENT '服务费率',
+  `agreemt` varchar(200) DEFAULT NULL COMMENT '居间服务和费率',
   `people` varchar(200) DEFAULT NULL COMMENT '适合人群',
   `state` char(1) DEFAULT NULL COMMENT '是否有效：1有效0无效',
-  `logo` varchar(100) DEFAULT NULL COMMENT 'logo',
   `sort` int(11) DEFAULT NULL COMMENT '排序',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='六漠产品介绍';
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='六漠产品介绍';
+
+/*Table structure for table `lm_recruit` */
+
+DROP TABLE IF EXISTS `lm_recruit`;
+
+CREATE TABLE `lm_recruit` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `workplace` varchar(50) DEFAULT NULL COMMENT '工作地点',
+  `rec_type` varchar(10) DEFAULT NULL COMMENT '招聘类型（01社会招聘02校园招聘）',
+  `category` varchar(10) DEFAULT NULL COMMENT '职位分类',
+  `job_title` varchar(100) DEFAULT NULL COMMENT '职位名称',
+  `job_nature` varchar(100) DEFAULT NULL COMMENT '工作性质',
+  `salary_range` varchar(100) DEFAULT NULL COMMENT '薪资范围',
+  `hiring` varchar(50) DEFAULT NULL COMMENT '招聘人数',
+  `job_duty` varchar(4000) DEFAULT NULL COMMENT '工作职责',
+  `job_quality` varchar(4000) DEFAULT NULL COMMENT '任职资格',
+  `state` char(1) DEFAULT NULL COMMENT '是否有效（1有效0无效）',
+  `rel_date` date DEFAULT NULL COMMENT '发布时间',
+  `sort` int(11) DEFAULT NULL COMMENT '排序',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='六漠招聘';
+
+/*Table structure for table `lm_release` */
 
 DROP TABLE IF EXISTS `lm_release`;
 
 CREATE TABLE `lm_release` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `category` varchar(50) DEFAULT NULL COMMENT '种类',
-  `brief` varchar(100) DEFAULT NULL COMMENT '发布简介',
-  `title` varchar(100) DEFAULT NULL COMMENT '发布标题',
-  `content` varchar(2000) DEFAULT NULL COMMENT '发布内容',
-  `logo` mediumblob COMMENT 'logo',
+  `category` varchar(10) DEFAULT NULL COMMENT '发布类型（01新闻发布；02媒体报道）',
+  `thumbnail` bigint(20) DEFAULT NULL COMMENT '缩略图ID',
+  `title` varchar(150) DEFAULT NULL COMMENT '发布标题',
+  `brief` varchar(300) DEFAULT NULL COMMENT '发布简介',
+  `original` varchar(100) DEFAULT NULL COMMENT '原创者',
+  `stick` char(1) DEFAULT NULL COMMENT '是否置顶（1是；0否）',
+  `state` char(1) DEFAULT NULL COMMENT '是否有效（1是；0否）',
+  `out_chain` varchar(300) DEFAULT NULL COMMENT '外部链接',
+  `content` mediumtext COMMENT '新闻内容',
+  `terminal` varchar(10) DEFAULT NULL COMMENT '发布终端（00全部；01PC；02MB）',
   `sort` int(11) DEFAULT NULL COMMENT '排序',
+  `release_date` date DEFAULT NULL COMMENT '发布日期',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
--- yiyun/sql/02.sql
+++ yiyun/sql/02.sql
@@ -1,24 +1,8 @@
 USE `yiyun`;
 
-insert into lm_product (title, content, agreemt, rate, people, state, logo, sort, create_time, update_time) values('银行抵押贷款','提供银行抵押贷款居间服务，为客户匹配最优银行，解决客户资金需求。','居间服务','2%','名下有可抵押房产的客户。','1','/static/img/pro_topic_bg01.4548e6d.png','1','2018-06-23 03:59:33','2018-06-23 04:41:56');
-insert into lm_product (title, content, agreemt, rate, people, state, logo, sort, create_time, update_time) values('银行信用贷款','提供银行抵押贷款居间服务，为客户匹配最优银行，解决客户资金需求。','居间服务','2%','名下有可抵押房产的客户。','1','/static/img/pro_topic_bg02.4001beb.png','2','2018-06-23 05:14:16','2018-06-23 05:55:58');
-insert into lm_product (title, content, agreemt, rate, people, state, logo, sort, create_time, update_time) values('银行按揭贷款','提供银行抵押贷款居间服务，为客户匹配最优银行，解决客户资金需求。','居间服务','2%','名下有可抵押房产的客户。','1','/static/img/pro_topic_bg03.458b8a4.png','3','2018-06-23 04:50:07','2018-06-23 05:14:39');
+/*Data for the table `lm_product` */
 
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('256','0','六漠科技','','','0','fa fa-globe','600',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('257','256','产品维护','/liumo/lmProduct','liumo:lmProduct:lmProduct','1','fa fa-clone','10',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('258','257','增加','/liumo/lmProduct/add','liumo:lmProduct:add','2','','1',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('259','257','编辑','/liumo/lmProduct/edit','liumo:lmProduct:edit','2','','2',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('260','257','删除','/liumo/lmProduct/remove','liumo:lmProduct:remove','2','','3',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('261','257','批量删除','/liumo/lmProduct/batchRemove','liumo:lmProduct:batchRemove','2','','4',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('262','256','发布维护','/liumo/lmRelease','liumo:lmRelease:lmRelease','1','fa fa-clone','20',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('263','262','增加','/liumo/lmRelease/add','liumo:lmRelease:add','2','','1',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('264','262','编辑','/liumo/lmRelease/edit','liumo:lmRelease:edit','2','','2',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('265','262','删除','/liumo/lmRelease/remove','liumo:lmRelease:remove','2','','3',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('266','262','批量删除','/liumo/lmRelease/batchRemove','liumo:lmRelease:batchRemove','2','','4',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('267','256','banner维护','/liumo/lmFile','liumo:lmFile:lmFile','1','fa fa-clone','20',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('268','267','增加','/liumo/lmFile/add','liumo:lmFile:add','2','','1',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('269','267','编辑','/liumo/lmFile/edit','liumo:lmFile:edit','2','','2',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('270','267','删除','/liumo/lmFile/remove','liumo:lmFile:remove','2','','3',NULL,NULL);
-insert into `sys_menu` (`menu_id`, `parent_id`, `name`, `url`, `perms`, `type`, `icon`, `order_num`, `gmt_create`, `gmt_modified`) values('271','267','批量删除','/liumo/lmFile/batchRemove','liumo:lmFile:batchRemove','2','','4',NULL,NULL);
+insert  into `lm_product`(`id`,`prd_type`,`title`,`content`,`agreemt`,`people`,`state`,`sort`,`create_time`,`update_time`) values (1,'A','银行抵押贷款','提供银行抵押贷款居间服务，为客户匹配最优银行，解决客户资金需求。','居间服务费：2%/次','名下有可抵押房产的客户。','1',1,'2018-06-23 03:59:33','2018-07-05 19:58:46'),(2,'B','银行信用贷款','提供银行抵押贷款居间服务，为客户匹配最优银行，解决客户资金需求。','居间服务费：2%/次','名下有可抵押房产的客户。','1',2,'2018-06-23 05:14:16','2018-07-05 19:58:55'),(3,'C','银行按揭贷款','提供银行抵押贷款居间服务，为客户匹配最优银行，解决客户资金需求。','居间服务费：2%/次','名下有可抵押房产的客户。','1',3,'2018-06-23 04:50:07','2018-07-05 19:59:04');
+
 
 commit;
